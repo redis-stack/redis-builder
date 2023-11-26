6d7086da7214df60839c7b2c8adb50001b90e8f0
Chayim I. Kirshen <c@kirshen.com>
2023-11-26 13:30:37 +0200

    intel fix

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
--c redis/src make all \
+make -C redis/src make all \
     BUILD_TLS=yes \
     FINAL_LIBS="-lm -ldl ../deps/hiredis/libhiredis_ssl.a /usr/local/opt/openssl/lib/libssl.a /usr/local/opt/openssl/lib/libcrypto.a"
