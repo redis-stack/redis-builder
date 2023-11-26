ffdc4553ca1f24e77aea2c7f38528e8b790925d4
Chayim I. Kirshen <c@kirshen.com>
2023-11-26 13:17:30 +0200

    path for make

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-make all \
+-c redis/src make all \
     BUILD_TLS=yes \
     FINAL_LIBS="-lm -ldl ../deps/hiredis/libhiredis_ssl.a /usr/local/opt/openssl/lib/libssl.a /usr/local/opt/openssl/lib/libcrypto.a"
